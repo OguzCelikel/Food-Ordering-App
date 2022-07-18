eace64bef683c34b8ad2dca7d64aa9572d4b2837
Ömer <omeroguzcelikel@gmail.com>
2022-07-18 18:12:16 +0300

    lunchScreenDone

---
+++ FoodOrderingApp/FoodOrderingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrderingApp
+//
+//  Created by Ömer Oğuz Çelikel on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
